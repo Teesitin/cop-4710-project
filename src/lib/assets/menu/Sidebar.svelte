670b5233bd3c3d25aa7df78ccbd0d43ca8b47a07
DeLayne Russell <teesitin.russell@gmail.com>
2026-04-16 19:13:57 -0400

    initial start and common theme layout
     - created theme layout
     - added sidebar
     - created dashbaord

---
+++ src/lib/assets/menu/Sidebar.svelte
@@ -0,0 +1,10 @@
+<div class="w-64 bg-gray-900 text-white p-4">
+    <img src="/techbro-logo.png" alt="logo" class="mb-4">
+
+    <ul class="space-y-2">
+        <li><a href="/" class="block p-2 rounded hover:bg-gray-700">Dashboard</a></li>
+        <li><a href="/applicants" class="block p-2 rounded hover:bg-gray-700">Applicants</a></li>
+        <li><a href="/jobs" class="block p-2 rounded hover:bg-gray-700">Jobs</a></li>
+        <li><a href="/settings" class="block p-2 rounded hover:bg-gray-700">Settings</a></li>
+    </ul>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import '../app.css';
+    import Sidebar from '$lib/assets/menu/Sidebar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head><link rel="icon" href="/techbro-logo-fav.png" /></svelte:head>
+
+<div class="flex h-screen">
+    <!-- Sidebar -->
+	<Sidebar/>
+
+    <!-- Content -->
+    <div class="flex-1 p-6 bg-gray-100">
+        {@render children()}
+    </div>
+</div>
