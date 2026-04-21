38380e7cdb7f12e0e3d6fab2e34956eece2cdfe1
Endermnce <moscosoleon2006@gmail.com>
2026-04-21 14:39:40 -0400

    new interviews+bricking jobs

---
+++ src/routes/interviews/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let jobs = [
+        { title: "Software Engineer", company: "Google" },
+        { title: "Frontend Dev", company: "Apple" }
+    ];
+</script>
+
+<table class="min-w-full border border-gray-300">
+    <thead class="bg-gray-900 text-white">
+        <tr>
+            <th class="px-4 py-2 text-left">Job Title</th>
+            <th class="px-4 py-2 text-left">Company</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {#each jobs as job}
+            <tr class="border-t">
+                <td class="px-4 py-2">{job.title}</td>
+                <td class="px-4 py-2">{job.company}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
